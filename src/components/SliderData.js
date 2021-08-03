d22b1a0a6c121d2a791a9d668337d85ef64b68dc
MinhChinh <minhchinh140901@gmail.com>
2021-08-03 16:30:15 +0200

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1627990282816-21d33cb33136?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80'
+    }, 
+    {
+        image: 'https://images.unsplash.com/photo-1627729115394-16a6019b4064?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1627994094964-dd7a6a81e11f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1627990910996-e614cf3f4668?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1586161713586-d0055e438da4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
+    }
+]
